f9dbcf201f121f0d2c60e0fd2f590c8c8065d43d
Taiwooye <Oyedokuntaiwo96@gmail.com>
2023-04-05 13:35:19 +0100

    netflix clone

---
+++ index.js
@@ -0,0 +1,183 @@
+let email = document.getElementById("email");
+let display = document.getElementById("show");
+
+const myModal = new bootstrap.Modal(document.getElementById("exampleModal"));
+// myModal.show();
+
+function next() {
+  if (email.value == "Oyedokuntaiwo96@gmail.com") {
+    // display.innerHTML = 'welcome to login page'
+    window.open("show.html", "_self");
+
+    localStorage.setItem("id", email.value);
+    localStorage.setItem(email.value, JSON.stringify([]));
+  } else if (email.value == "") {
+    display.innerHTML = "Input field is empty";
+  } else {
+    alert("Account Not Registered");
+
+    window.open("Error.html", "_self");
+  }
+}
+
+let movies = [];
+
+console.log(window.location.href);
+
+if (window.location.href == "http://127.0.0.1:5500/show.html") {
+  load();
+}
+
+//https://api.themoviedb.org/3/movie/76600/videos?api_key=f7e65740c28f0e6ddf5f412c8e4df4f8&language=en-US
+
+async function videDetails(id) {
+  let output = [];
+  output = fetch(
+    `https://api.themoviedb.org/3/movie/${id}/videos?api_key=f7e65740c28f0e6ddf5f412c8e4df4f8&language=en-US`
+  )
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      return data.results.filter(
+        (item) => item.type == "Trailer" && item.name == "Official Trailer"
+      );
+    });
+
+  return output;
+}
+
+function load() {
+  fetch(
+    "https://api.themoviedb.org/3/trending/all/day?api_key=f7e65740c28f0e6ddf5f412c8e4df4f8"
+  )
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      watchmovie(data.results);
+
+      // console.log(data.results);
+    });
+}
+
+let moviecontent = document.querySelector(".moviecontent");
+
+function watchmovie(movies) {
+
+  let j = 0;
+  for (let i = 0; i < movies.length; i++) {
+    let content = document.createElement("div");
+
+    let path = "https://image.tmdb.org/t/p/original";
+
+    if (movies[i].original_title != undefined) {
+      let img = movies[i].backdrop_path;
+      let date = movies[i].release_date;
+      let title = movies[i].original_title;
+      let overview = movies[i].overview;
+      let mvId = movies[i].id;
+      //let other=movies[i].original_name;
+
+      content.innerHTML = `<div class="movie-box">
+      <div class="pic">
+        <img src="${path}${img}" class="movie-img">
+      </div>
+      <h2 class="movie-title">${title.substr(0, 18)}</h2>
+    <div class+"like_contain">  <span class="movie-price">${date} </span>
+    <i class="fa-regular fa-heart like" ></i></div>
+    
+    </div>`;
+
+      moviecontent.append(content);
+
+      let likeMovie = document.querySelectorAll(".like");
+
+      likeMovie[j].addEventListener("click", function (index, j) {
+        let userid = localStorage.id;
+
+        let movies = JSON.parse(localStorage.getItem(userid));
+        //console.log(movies);
+
+        if (movies.includes(title)) {
+          console.log(j);
+          let pos = movies.indexOf(title);
+          movies.splice(pos, 1);
+          localStorage.setItem(userid, JSON.stringify(movies));
+          this.classList.remove("fa-solid");
+          this.classList.add("fa-regular");
+          console.log(this);
+        } else {
+          this.classList.remove("fa-regular");
+          this.classList.add("fa-solid");
+          console.log(this);
+          movies.push(title);
+          localStorage.setItem(userid, JSON.stringify(movies));
+        }
+      });
+
+      let moveImg = document.querySelectorAll(".movie-img");
+
+      moveImg[j].onclick = async function () {
+        // console.log("Image clicked");
+
+        // document.getElementById("image").innerHTML = img;
+        // document.getElementById("titles").innerHTML = title;
+        // myModal.show();
+
+        await Showdetails(title, img, overview, mvId);
+      };
+
+      j++;
+    }
+  }
+}
+
+async function Showdetails(title, img, overview, id) {
+  //moviecontent.innerHTML = "";
+
+  let path = "https://image.tmdb.org/t/p/original";
+  let youtube_path = "https://www.youtube.com/embed/";
+
+  let watch = await videDetails(id).then((data) => {
+    document.getElementById("image").src = path + img;
+    document.getElementById("image").style.display = "none";
+    document.getElementById("iframe").src = youtube_path + data[0].key;
+
+    console.log(data);
+  });
+
+  //console.log(watch);
+
+  document.getElementById("titles").innerHTML = title.substr(0, 18);
+  document.getElementById("overview").innerHTML = overview;
+  myModal.show();
+}
+
+//https://www.youtube.com/embed/d9MyW72ELq0
+//seeVideo();
+
+async function seeVideo() {
+  let watch = await videDetails(76600).then((data) => console.log(data));
+
+  //return console.log(watch);
+}
+
+var stopVideo = function () {
+  var iframe = document.querySelector("iframe");
+  var video = document.querySelector("video");
+  if (iframe) {
+    iframe.src = "";
+    //iframe.src = iframeSrc;
+  }
+  if (video) {
+    video.pause();
+  }
+};
+let close = document.querySelector(".btn-close");
+close.addEventListener("click", function () {
+  //console.log("close");
+  //stopVideo;
+
+  document.getElementById("iframe").src = "";
+});
